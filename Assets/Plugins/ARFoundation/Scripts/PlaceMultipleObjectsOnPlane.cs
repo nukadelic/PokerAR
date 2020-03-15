768b407b009d7fee4960250cacec190e3c726b40
Nukadelic <vladik.voina@gmail.com>
2020-03-15 07:38:38 +0700

    initial commit

---
+++ Assets/Plugins/ARFoundation/Scripts/PlaceMultipleObjectsOnPlane.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceMultipleObjectsOnPlane : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_PlacedPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedPrefab
+    {
+        get { return m_PlacedPrefab; }
+        set { m_PlacedPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedObject { get; private set; }
+
+    /// <summary>
+    /// Invoked whenever an object is placed in on a plane.
+    /// </summary>
+    public static event Action onPlacedObject;
+
+    ARRaycastManager m_RaycastManager;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+                {
+                    Pose hitPose = s_Hits[0].pose;
+
+                    var rot = hitPose.rotation * Quaternion.Euler(0, 180f, 0);
+
+                    spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, rot);
+
+                    spawnedObject.transform.localScale = new Vector3( 0.064f, 0.089f, 0.1f );
+
+                    spawnedObject.transform.localScale *= 0.65f;
+
+                    onPlacedObject?.Invoke();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Project/Scripts/PlaceCardOnSurface.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlaceCardOnSurface : MonoBehaviour
+{
+    public static event System.Action<GameObject> OnCardPlaced;
+
+    ARRaycastManager raycaster;
+
+    public GameObject[] cards;
+
+    List<GameObject> spawned;
+    
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void Start()
+    {
+        spawned = new List<GameObject>();
+
+        raycaster = FindObjectOfType<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if( Input.touchCount < 1 ) return;
+
+        Touch touch = Input.GetTouch(0);
+
+        if ( touch.phase != TouchPhase.Began ) return;
+
+        bool hit = raycaster.Raycast( touch.position, hits, TrackableType.PlaneWithinPolygon );
+
+        if( ! hit || hits.Count < 1 ) return;
+
+        Pose pose = hits[0].pose;
+
+        var pos = pose.position;
+
+        var rot = pose.rotation * Quaternion.Euler(0, 180f, 0);
+
+        var card = cards[Random.Range(0, cards.Length)];
+
+        var go = Instantiate(card, pos, rot);
+
+        go.transform.localScale = new Vector3( 0.064f, 0.089f, 0.1f ) * 0.65f;
+
+        spawned.Add( go );
+
+        OnCardPlaced?.Invoke( go );
+    }
+}
